fcbd685688685e34f8d3de8a2b2a818fd618312c
lewypopescu <popesculewy@gmail.com>
2024-07-07 10:21:55 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,11 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 100,
         color: '#010101',
       }}
     >
-      React homework start go
+      React go
     </div>
   );
 };
